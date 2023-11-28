cf602c767622ea88656440713a7a5c587d9a9b98
Anton <antonymousfeedback@gmail.com>
2023-11-28 19:17:51 +0100

    Add main.py with producer code

---
+++ main.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+import queue
+import numpy as np
+
+from lib.ws_server import run_server
+from multiprocessing import Process, Queue
+
+from BlazeposeDepthai import BlazeposeDepthai
+
+if __name__ == '__main__':
+    tracker = BlazeposeDepthai(pd_model='models/pose_detection_sh6.blob',
+                               lm_model='lite',
+                               smoothing=True,
+                               xyz=True,
+                               crop=False,
+                               internal_fps=25,
+                               internal_frame_height=400,
+                               force_detection=False,
+                               stats=True,
+                               trace=True)
+
+    cfg_rcv_queue = Queue()
+    body_send_queue = Queue(1)
+    p = Process(target=run_server, args=(cfg_rcv_queue, body_send_queue,))
+    p.start()
+
+    while True:
+        # Run blazepose on next frame
+        frame, body = tracker.next_frame()
+        # Parse results
+        if body is not None:
+            landmarks = body.landmarks_world
+            xyz = body.xyz
+            xyz_ref = body.xyz_ref
+            body_payload = np.insert(landmarks, 0, xyz, 0).tobytes()
+            xyz_ref_bytes = bytes(bytearray(xyz_ref, 'ascii'))
+            body_payload = b"".join([body_payload, xyz_ref_bytes])
+            # Send results via Websockets
+            try:
+                body_send_queue.put(bytes(body_payload))
+            except queue.Full:
+                pass
+
+    p.kill()
+    renderer.exit()
+    tracker.exit()
